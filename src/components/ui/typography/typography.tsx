198597c6cf86f3abbef61d1d62b7681b690c9236
Artur Khareksian <r2mail1122@gmail.com>
2023-10-15 22:47:00 +0300

    Typography

--- src/components/ui/typography/typography.tsx
+++ src/components/ui/typography/typography.tsx
@@ -1,1 +1,33 @@
-export default '#'
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+
+import { clsx } from 'clsx'
+
+import s from './typography.module.scss'
+
+type TypographyProps<T extends ElementType = 'p'> = {
+  as?: T
+  children: ReactNode
+  className?: string
+  variant?:
+    | 'body1'
+    | 'body2'
+    | 'caption'
+    | 'h1'
+    | 'h2'
+    | 'h3'
+    | 'large'
+    | 'link1'
+    | 'link2'
+    | 'overline'
+    | 'subtitle1'
+    | 'subtitle2'
+} & ComponentPropsWithoutRef<T>
+
+export const Typography = <T extends ElementType = 'p'>(
+  props: TypographyProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof TypographyProps<T>>
+) => {
+  const { as: Component = 'span', className, variant = 'body1', ...rest } = props
+  const classNames = clsx(s[variant], className)
+
+  return <Component className={classNames} {...rest} />
+}
