97c167967d511ef957de3943c434ab6f2fc34af5
Artur Khareksian <r2mail1122@gmail.com>
2023-10-15 10:49:03 +0300

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { StrictMode } from 'react'
 import { App } from '@/App'
 import { createRoot } from 'react-dom/client'
 
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
 import './index.css'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
