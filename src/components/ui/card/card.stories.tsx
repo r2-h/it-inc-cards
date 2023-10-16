db6140c4dd258515e9321028376e2ebeb8df2c27
Artur Khareksian <r2mail1122@gmail.com>
2023-10-16 11:56:31 +0300

    Card

--- src/components/ui/card/card.stories.tsx
+++ src/components/ui/card/card.stories.tsx
@@ -1,16 +1,16 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import {Card} from "@/components/ui/card/card.tsx";
 
+import { Card } from '@/components/ui/card/card'
 
 const meta = {
-    component: Card,
-    tags: ['autodocs'],
-    title: 'Components/Card',
+  component: Card,
+  tags: ['autodocs'],
+  title: 'Components/Card',
 } satisfies Meta<typeof Card>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 export const CardStory: Story = {
-    args: {},
+  args: {},
 }
--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -1,16 +1,23 @@
+import { ComponentPropsWithoutRef, FC } from 'react'
+// import { ElementType } from 'react'
+
 import s from './card.module.scss'
-import {ComponentPropsWithoutRef, ElementType} from "react";
 
-export type CardProps<T extends ElementType = 'div'> = {
-    as?: T
-    className?: string
-    fullWidth?: boolean
-} & ComponentPropsWithoutRef<T>
-
-export const Card = <T extends ElementType = 'div'>(
-    props: CardProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof CardProps<T>>
-) => {
-    const { as: Component = 'div', className, fullWidth, ...rest } = props
-
-    return <Component className={`${s.card} ${className}`} {...rest} />
+type CardProps = {} & ComponentPropsWithoutRef<'div'>
+export const Card: FC<CardProps> = ({ className, ...rest }) => {
+  return <div className={`${s.card} ${className}`} {...rest}></div>
 }
+
+// export type CardProps<T extends ElementType = 'div'> = {
+//     as?: T
+//     className?: string
+//     fullWidth?: boolean
+// } & ComponentPropsWithoutRef<T>
+//
+// export const Card = <T extends ElementType = 'div'>(
+//     props: CardProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof CardProps<T>>
+// ) => {
+//     const { as: Component = 'div', className, fullWidth, ...rest } = props
+//
+//     return <Component className={`${s.card} ${className}`} {...rest} />
+// }
