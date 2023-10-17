437fc4f6476194ed1dee6c1052dbe570cf64fe9c
Artur Khareksian <r2mail1122@gmail.com>
2023-10-17 20:40:32 +0300

    CheckBox

--- src/components/ui/check-box/check-box.stories.tsx
+++ src/components/ui/check-box/check-box.stories.tsx
@@ -1,25 +1,26 @@
-import type {Meta, StoryObj} from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react'
 
-import {CheckboxDemo} from '@/components/ui/check-box/check-box'
+import { CheckboxDemo } from '@/components/ui/check-box/check-box'
 
 const meta = {
-    component: CheckboxDemo,
-    tags: ['autodocs'],
-    title: 'Components/CheckboxDemo',
+  component: CheckboxDemo,
+  tags: ['autodocs'],
+  title: 'Components/CheckboxDemo',
 } satisfies Meta<typeof CheckboxDemo>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const CheckboxWithLabel: Story = {
-    args: {
-        label: 'Check-box',
-        disabled: false,
-    },
+export const Checkbox: Story = {
+  args: {
+    onChange: () => {},
+  },
 }
 
-export const Checkbox: Story = {
-    args: {
-        disabled: false,
-    },
+export const CheckboxWithLabel: Story = {
+  args: {
+    disabled: false,
+    label: 'Check-box',
+    onChange: () => {},
+  },
 }
--- src/components/ui/check-box/check-box.tsx
+++ src/components/ui/check-box/check-box.tsx
@@ -1,30 +1,51 @@
-import {FC} from 'react'
-import CheckboxImg from '@/assets/checkboxImg'
-import CheckboxDoneImg from '@/assets/checkboxDone'
-import {Typography} from '@/components/ui/typography'
+import { FC } from 'react'
 
+import CheckboxDoneImg from '@/assets/checkboxDone'
+import CheckboxImg from '@/assets/checkboxImg'
+import { Typography } from '@/components/ui/typography'
 import * as Checkbox from '@radix-ui/react-checkbox'
 
 import s from './check-box.module.scss'
 
 type CheckboxDemoProps = {
-    label?: string
-    disabled?: boolean
+  disabled?: boolean
+  label?: string
+  onChange: (checked: boolean) => void
+  required?: boolean
 }
 
-export const CheckboxDemo: FC<CheckboxDemoProps> = ({disabled = false, label = ''}) => {
-    return (
-        <div className={s.wrapper}>
-            <Checkbox.Root disabled={disabled} className={s.root}>
-                <CheckboxImg className={s.icon}/>
-                <Checkbox.Indicator>
-                    <CheckboxDoneImg className={s.icon}/>
-                </Checkbox.Indicator>
-            </Checkbox.Root>
+export const CheckboxDemo: FC<CheckboxDemoProps> = ({
+  disabled = false,
+  label = '',
+  onChange,
+  // required,
+}) => {
+  return (
+    <div className={s.wrapper}>
+      <Checkbox.Root
+        className={s.root}
+        // defaultChecked
+        disabled={disabled}
+        id={'c1'}
+        onCheckedChange={onChange}
+        // required={required}
+      >
+        <CheckboxImg className={s.icon} />
+        <Checkbox.Indicator>
+          <CheckboxDoneImg className={s.icon} />
+        </Checkbox.Indicator>
+      </Checkbox.Root>
 
-            {label &&
-                <Typography className={disabled ? `${s.checkboxLabel} ${s.checkboxLabelDisabled}` : s.checkboxLabel}
-                            variant={'body2'}>{label}</Typography>}
-        </div>
-    )
+      {label && (
+        <Typography
+          as={'label'}
+          className={disabled ? `${s.checkboxLabel} ${s.checkboxLabelDisabled}` : s.checkboxLabel}
+          htmlFor={'c1'}
+          variant={'body2'}
+        >
+          {label}
+        </Typography>
+      )}
+    </div>
+  )
 }
