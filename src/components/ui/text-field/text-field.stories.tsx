dea36e219642e4d6229cf5c3589ecdb6af6a2012
Artur Khareksian <r2mail1122@gmail.com>
2023-10-23 12:51:09 +0300

    login-form

--- src/components/ui/text-field/text-field.stories.tsx
+++ src/components/ui/text-field/text-field.stories.tsx
@@ -14,19 +14,19 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
   args: {
     disabled: false,
+    fullWidth: false,
     label: 'Input',
     placeholder: 'Input',
-    fullWidth: false,
   },
 }
 
 export const Password: Story = {
   args: {
     disabled: false,
+    fullWidth: false,
     label: 'Input',
     placeholder: 'Password',
     type: 'password',
-    fullWidth: false,
   },
 }
 
@@ -34,16 +34,16 @@ export const Error: Story = {
   args: {
     disabled: false,
     errorMessage: 'Error message',
+    fullWidth: false,
     label: 'Input with error',
     value: 'Wrong value',
-    fullWidth: false,
   },
 }
 export const Search: Story = {
   args: {
     disabled: false,
+    fullWidth: false,
     placeholder: 'Input search',
     type: 'search',
-    fullWidth: false,
   },
 }
