e45ce0ffb2754af9fdd600e227be1952d9ddf7b4
Mannomann <krivmann00@gmail.com>
2023-10-26 22:55:50 +0200

    last fix

--- src/components/ui/slider/slider.stories.tsx
+++ src/components/ui/slider/slider.stories.tsx
@@ -15,9 +15,6 @@ type Story = StoryObj<typeof meta>
 
 export const SliderStory: Story = {
   args: {
-    max: 100,
-    min: 0,
-    minStepsBetweenThumbs: 1,
     value: [0, 100],
   },
   render: args => <StorySlider {...args} />,
@@ -27,14 +24,13 @@ const StorySlider = (args: SliderPropsType) => {
   const [state, setState] = useState(args.value)
 
   useEffect(() => {
-    setState(args.value)
-    if (args.max && args.min) {
-      if (args.value[0] < args.min) {
-        setState(prev => [args.min!, prev[1]])
-      }
-      if (args.value[1] > args.max) {
-        setState(prev => [prev[0], args.max!])
-      }
+    setState(prev => (prev.toString() !== args.value.toString() ? args.value : prev))
+
+    if (args.min && args.value[0] < args.min) {
+      setState(prev => [args.min!, prev[1]])
+    }
+    if (args.max && args.value[1] > args.max) {
+      setState(prev => [prev[0], args.max!])
     }
   }, [args])
 
