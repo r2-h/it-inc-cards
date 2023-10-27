346a377b838b5e5afc4ec5bb1c0791fbbb581da3
Artur Khareksian <r2mail1122@gmail.com>
2023-10-27 11:16:30 +0300

    slider edit

--- src/components/ui/slider/slider.stories.tsx
+++ src/components/ui/slider/slider.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import { useEffect, useState } from 'react'
 
-import { Slider, SliderPropsType } from '@/components/ui/slider/slider'
+import { Slider, SliderProps } from '@/components/ui/slider/slider'
 
 const meta = {
   component: Slider,
@@ -13,34 +13,27 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const SliderStory: Story = {
+export const Default: Story = {
   args: {
-    value: [0, 100],
+    value: [25, 75],
   },
   render: args => <StorySlider {...args} />,
 }
 
-const StorySlider = (args: SliderPropsType) => {
+const StorySlider = (args: SliderProps) => {
   const [state, setState] = useState(args.value)
 
   useEffect(() => {
     setState(prev => (prev.toString() !== args.value.toString() ? args.value : prev))
 
-    if (args.min && args.value[0] < args.min) {
-      setState(prev => [args.min!, prev[1]])
-    }
-    if (args.max && args.value[1] > args.max) {
-      setState(prev => [prev[0], args.max!])
-    }
+    // if (args.min && args.value[0] < args.min) {
+    //   console.log(args.min)
+    //   setState(prev => [args.min!, prev[1]])
+    // }
+    // if (args.max && args.value[1] > args.max) {
+    //   setState(prev => [prev[0], args.max!])
+    // }
   }, [args])
 
-  return (
-    <Slider
-      max={args.max}
-      min={args.min}
-      minStepsBetweenThumbs={args.minStepsBetweenThumbs}
-      onValueChange={(value: number[]) => setState(value)}
-      value={state}
-    />
-  )
+  return <Slider {...args} onValueChange={(value: number[]) => setState(value)} value={state} />
 }
--- src/components/ui/slider/slider.tsx
+++ src/components/ui/slider/slider.tsx
@@ -1,40 +1,47 @@
 import * as RadixSlider from '@radix-ui/react-slider'
+import clsx from 'clsx'
 
 import s from './slider.module.scss'
 
-export type SliderPropsType = {
+export type SliderProps = {
+  className?: string
   max?: number
   min?: number
   minStepsBetweenThumbs?: number
   onValueChange: (value: number[]) => void
+  step?: number
   value: number[]
 }
 export const Slider = ({
+  className,
   max,
   min,
   minStepsBetweenThumbs = 1,
   onValueChange,
+  step = 1,
   value,
-}: SliderPropsType) => {
+}: SliderProps) => {
+  const containerCN = clsx(s.container, className)
+
   return (
-    <div className={s.container}>
-      <div className={s.min}>{value[0] | 0}</div>
+    <div className={containerCN}>
+      <div className={s.number}>{value[0]}</div>
       <RadixSlider.Root
-        className={s.SliderRoot}
+        className={s.sliderRoot}
         max={max}
         min={min}
         minStepsBetweenThumbs={minStepsBetweenThumbs}
         onValueChange={onValueChange}
-        step={1}
+        step={step}
         value={value}
       >
-        <RadixSlider.Track className={s.SliderTrack}>
-          <RadixSlider.Range className={s.SliderRange} />
+        <RadixSlider.Track className={s.sliderTrack}>
+          <RadixSlider.Range className={s.sliderRange} />
         </RadixSlider.Track>
-        <RadixSlider.Thumb className={s.SliderThumb} />
-        <RadixSlider.Thumb className={s.SliderThumb} />
+        <RadixSlider.Thumb className={s.sliderThumb} />
+        <RadixSlider.Thumb className={s.sliderThumb} />
       </RadixSlider.Root>
-      <div className={s.max}>{value[1] | 0}</div>
+      <div className={s.number}>{value[1]}</div>
     </div>
   )
 }
