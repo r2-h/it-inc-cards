38e35aaac682c92f6e983ea0f5017bd3f92d17b3
Oksana <oksana.s.kovalchuk@gmail.com>
2023-11-11 10:51:56 +0300

    handling errors: create-new-password

--- src/pages/auth/create-password-page.tsx
+++ src/pages/auth/create-password-page.tsx
@@ -1,20 +1,23 @@
 import { useNavigate, useParams } from 'react-router-dom'
 
-import { CreateNewPassword, CreatePasswordFormValues } from '@/components/auth/create-new-password'
-import { useResetPasswordMutation } from '@/services'
+import { CreateNewPassword, CreatePasswordFormValues } from '@/components'
+import { ErrorType, authActions, useAppDispatch, useResetPasswordMutation } from '@/services'
 
 export const CreatePasswordPage = () => {
   const navigate = useNavigate()
   const { token } = useParams<{ token: string }>()
+  const dispatch = useAppDispatch()
 
   const [reset] = useResetPasswordMutation()
 
   const resetPasswordHandler = async ({ password }: CreatePasswordFormValues) => {
     try {
-      await reset({ password, token: token ?? '' })
+      await reset({ password, token: token ?? '' }).unwrap()
       navigate('/login')
-    } catch (e) {
-      console.log(e)
+    } catch (err) {
+      const error = err as ErrorType
+
+      dispatch(authActions.setError(error.data.message))
     }
   }
 
